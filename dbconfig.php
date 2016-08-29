12760ed925ff352b8a0045c6481ab04d6765c161
Carol Pereira <carolnicolapereira@gmail.com>
2016-08-29 15:48:29 +0530

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,29 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
+error_reporting(E_ERROR | E_PARSE);
+
+$node1="NODE1_IP:3306";
+$node2="NODE2_IP:3306";
+
+$user="root";
+$pass="root";
 $datbase = "dbtuts";
-mysql_connect($host,$user,$password);
+
+# Checking db connections
+# Load balancing can be used here
+if( !mysql_connect($node1, $user, $pass) ){
+
+#if( !$conn_node1 ){
+        #echo "Node 1" . mysql_error();
+      $conn_node2 = mysql_connect($node2, $user, $pass);
+      if (!$conn_node2){
+                echo "Not been able to connect to both nodes " . mysql_error();
+      }else{
+        echo "Connected node 2";
+      }
+}else{
+        echo "Connected to node1";
+}
+
 mysql_select_db($datbase);
+
 ?>
