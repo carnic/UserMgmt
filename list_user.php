ead2ec802abce51dba243cc51a451e3f7db46dd0
Carol Pereira <carolnicolapereira@gmail.com>
2016-08-29 14:35:05 +0530

    Update list_user.php

--- list_user.php
+++ list_user.php
@@ -49,7 +49,7 @@ function delete_id(id)
  <div id="content">
     <table align="center">
     <tr>
-    <th colspan="5"><a href="add_data.php">add data here.</a></th>
+    <th colspan="5"><a href="user_signup.php">add data here.</a></th>
     </tr>
     <th>First Name</th>
     <th>Last Name</th>
@@ -67,7 +67,7 @@ function delete_id(id)
         <td><?php echo $row[2]; ?></td>
         <td><?php echo $row[3]; ?></td>
   <td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
-        <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
+        <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_delete.png" align="DELETE" /></a></td>
         </tr>
         <?php
  }
